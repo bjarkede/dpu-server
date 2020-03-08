b63131278227d7c58638a57fccd86852cc8088a5
Bjarke Damsgaard Eriksen <bde@au.dk>
2020-03-08 14:19:36 +0100

    Add Geometry.cs

---
+++ dpu-server/Geometry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dpu_server
+{
+    class Point2D
+    {
+        double x, y, distance;
+        int classification;
+
+        double norm2() { return (x * x) + (y * y); }
+        double norm() { return Math.Sqrt(norm2()); }
+
+        public static bool operator <(Point2D p1, Point2D p2)
+        {
+            return p1.x < p2.x || (p1.x == p2.x && p1.y < p2.y);
+        }
+        public static bool operator >(Point2D p1, Point2D p2)
+        {
+            return p1.x > p2.x || (p1.x == p2.x && p1.y > p2.y);
+        }
+        public static Point2D operator +(Point2D p1, Point2D p2)
+        {
+            Point2D temp = new Point2D();
+            temp.x = p1.x + p2.x;
+            temp.y = p1.y + p2.y;
+            return temp;
+        }
+
+        public static Point2D operator -(Point2D p1, Point2D p2)
+        {
+            Point2D temp = new Point2D();
+            temp.x = p1.x - p2.x;
+            temp.y = p1.y - p2.y;
+            return temp;
+        }
+
+        public static Point2D operator /(Point2D p, double num)
+        {
+            Point2D temp = new Point2D();
+            temp.x = p.x/num;
+            temp.y = p.y/num;
+            return temp;
+        }
+
+        public static Point2D operator *(Point2D p, double num)
+        {
+            Point2D temp = new Point2D();
+            temp.x = p.x * num;
+            temp.y = p.y * num;
+            return temp;
+        }
+    }
+
+    class Circle2D
+    {
+        Point2D p;
+        double radius;
+    }
+}
