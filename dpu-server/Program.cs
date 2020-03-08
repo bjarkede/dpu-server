82665a2f6199cfc0f2840017cb27e59a3ce2b34b
Bjarke Damsgaard Eriksen <bde@au.dk>
2020-03-08 14:03:31 +0100

    Add an example TCP client.

--- dpu-server/Program.cs
+++ dpu-server/Program.cs
@@ -1,14 +1,81 @@
 ﻿using System;
-using System.Net.WebSockets;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
+using System.Text;
 
 namespace dpu_server
 {
-    class Program
+    public class SynchronousSocketClient
     {
-        static void Main(string[] args)
+
+        public static void StartClient()
         {
-            Console.WriteLine("Hello World!");
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = IPAddress.Parse("192.168.88.128");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 27015);
+
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes("PWD");
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.  
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("{0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.  
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static int Main(String[] args)
+        {
+            StartClient();
+            return 0;
         }
     }
 }
+
