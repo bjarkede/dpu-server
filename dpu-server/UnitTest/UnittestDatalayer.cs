5195fc85c3d7e35064d8374d057345630e1c7456
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2020-03-26 13:06:30 +0100

    Add Created testdatabase (requires further attention) and testcases

---
+++ dpu-server/UnitTest/UnittestDatalayer.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using NUnit.Framework;
+using dpu_server.UnitTest.Utilities;
+using dpu_server.DataLayer;
+using dpu_server.Configurations;
+
+namespace dpu_server.UnitTest
+{
+    [TestFixture]
+    public class UnittestDatalayer
+    {
+        public UnittestDatalayer()
+        {
+            using (var db = new FruitFlyContext(SQLContext.TestDbContextOptions()))
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+        }
+
+        [Test]
+        public void CreateReferencepoint()
+        {
+
+        }
+
+        [Test]
+        public void CreateHeatmap()
+        {
+
+        }
+
+        [Test]
+        public void RemoveReferencepoint()
+        {
+
+        }
+
+        [Test]
+        public void RemoveHeatmap()
+        {
+
+        }
+
+        [Test]
+        public void UpdateReferencepoint()
+        {
+
+        }
+
+        [Test]
+        public void UpdateHeatmap()
+        {
+
+        }
+
+        [Test]
+        public void DoesReferencepointExist()
+        {
+
+        }
+
+        [Test]
+        public void DoesHeatmapExist()
+        {
+
+        }
+
+        [Test]
+        public void UpdateReferencepointCategory()
+        {
+
+        }
+
+        [Test]
+        public void UpdateHeatmapStrength()
+        {
+
+        }
+    }
+}
---
+++ dpu-server/UnitTest/Utilities/SQLContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace dpu_server.UnitTest.Utilities
+{
+
+    public class SQLContext
+    {
+        public static DbContextOptions<FruitFlyContext> TestDbContextOptions()
+        {
+            //New service provider to create new SQL database for unit tests
+            var service = new ServiceCollection()
+                .AddEntityFrameworkSqlServer()
+                .BuildServiceProvider();
+
+            //New instance is created
+            var builder = new DbContextOptionsBuilder<FruitFlyContext>()
+                .UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=Unittestdatabase.mdf;Integrated Security=True;") //Properly not right
+                .UseInternalServiceProvider(service);
+
+            return builder.Options;
+        }
+    }
+}
